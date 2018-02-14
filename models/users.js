fe88a7eebf0a453dc54f6bd7f4ac7a028ea92b37
shisirT <shisirinatl@gmail.com>
2018-02-14 16:56:21 +0545

    mongoose continue

---
+++ models/users.js
@@ -0,0 +1,29 @@
+var mongoose = require ('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	firstName: String,
+	lastName: {
+		type: String
+	},
+	username: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: String,
+	email: {
+		type:String,
+		unique:true,
+		required: true
+	},
+	phoneNumber: Number,
+	activeStatus: Boolean,
+	address: String,
+	linkedIn_id: String
+},{
+	timestamps: true
+});
+
+var userModel = mongoose.model('user',userSchema);
+module.exports = userModel;
--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,27 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 var dbUrl = 'mongodb://127.0.0.1:27017'
 
+var UserModel = require('./../models/users');
 
+function validate(req){
+    req.checkBody('username','username is required').notEmpty();
+    req.checkBody('password','password is required').notEmpty();
+    req.checkBody('password','password should be 8 characters').isLength({min:8});
+    req.checkBody('password','password max 8 characters').isLength({max:12});
+//try assert inplace of checkBody //req.assert
+    var errors = req.validationErrors();
+
+    if(errors){
+        console.log('error occured validating form');
+        return (errors);
+        //return next(errors); //to end if there is no place to do next
+    }else{
+
+       return null;   
+    }
+}
 module.exports = function(){
-
-
 router.get('/',function(req,res,next){
-
 	console.log('this is home page');
 	//req.headers
 	//req.query
@@ -37,29 +52,16 @@ router.get('/',function(req,res,next){
     	message:'welcome to express js'
     });
     //searches index in views
-
-
 });
-
-
 router.post('/',function(req,res,next){
-
 	console.log('this is post page');
 	console.log('body>>>',req.body);
-
-	req.checkBody('username','username is required').notEmpty();
-    req.checkBody('password','password is required').notEmpty();
-    req.checkBody('password','password should be 8 characters').isLength({min:8});
-    req.checkBody('password','password max 8 characters').isLength({max:12});
-//try assert inplace of checkBody //req.assert
-    var errors = req.validationErrors();
-
-    if(errors){
-    	console.log('error occured validating form');
-    	return next(errors);
-    	//return next(errors); //to end if there is no place to do next
+    
+    var error = validate(req);
+    if(error){
+        return next(error)
     }
-
+/*	
     MongoClient.connect(dbUrl,function(err,dbClient){
         if(err){
             console.log('cannot connect to db');
@@ -85,8 +87,21 @@ router.post('/',function(req,res,next){
             
         }
     });
+*/
 
-
+var newUser = new UserModel();
+newUser.name = req.body.name,
+newUser.email = req.body.email,
+newUser.username = req.body.username,
+newUser.passsword = req.body.passsword
+newUser.save(function(err,savedUser){
+    if(err){
+        return next(err);
+    }else{
+        console.log('saved database');
+        res.json(savedUser);
+    }
+});
 });
 
 
