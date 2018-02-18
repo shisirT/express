7485f77a5329b42b8f936899f5a995eeed8e2a04
shisirT <shisirinatl@gmail.com>
2018-02-19 00:22:15 +0545

    token based authentication

--- routes/index.js
+++ routes/index.js
@@ -62,9 +62,12 @@ function map_user_req(user,userDetails){
 
 }
 
-module.exports = function(){
+module.exports = function(config){
   router.get('/',function(req,res,next){
     console.log('welcome to hamro app!');
+    res.json({
+      message:'welcome to hamro app'
+    });
   });
  
  router.post('/login',function(req,res,next){
