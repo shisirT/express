cf74d27c49134220a5dfa8afdc115a200fee7ec9
shisirT <shisirinatl@gmail.com>
2018-02-21 14:53:05 +0545

    product schema

--- models/products.js
+++ models/products.js
@@ -1,6 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+var locationSchema = new Schema({
+  city:String,
+  zone:String,
+  zipCode:String,
+  streetAddress:String,
+  houseNo: String
+});
+
+
 var productSchema = new Schema({
   category:{
   	type:String
@@ -12,9 +21,59 @@ var productSchema = new Schema({
   brand:{
   	type:String
   },
-  
-
-
+  measurementUnit: {
+  	type: String
+  },
+  price: {
+  	type: Number
+  },
+  color: {
+  	type: String
+  },
+  quantity: {
+  	type: Number
+  },
+  origin: {
+  	type: String
+  },
+  description: {
+  	type: String
+  },
+  attributes: {
+  	type: String
+  },
+  rating: {
+  	ENUM: ['1','2','3','4','5'],
+  	default:3
+  },
+  manuDate: {
+  	type: Date,
+  	default: Date.now()
+  },
+  dimensions: {
+  	type: String
+  },
+  imageName: [String],
+  distributors: [String],
+  discount: {
+  	type: String
+  },
+  delivery : {
+  	type: Boolean,
+  	default:false
+  },
+  feedbacks: {
+  	type: String
+  },
+  offers: {
+  	type: String
+  },
+  address: {
+  	type: String
+  },
+  tags: {
+  	type:String
+  }
 });
 
 
