86e656675c9aa5d61d94008ba5abfbf5fb7f81ea
shisirT <shisirinatl@gmail.com>
2018-02-25 10:58:58 +0545

    adding secret key in config

--- config/index.js
+++ config/index.js
@@ -6,7 +6,8 @@ module.exports.app = {
 }
 
 module.exports.mongodb = {
-	url:'mongodb://127.0.0.1:27017/abc'
+	localUrl:'mongodb://127.0.0.1:27017/abc',
+    mlabUrl: 'mongodb://shisir:test123456789@ds245518.mlab.com:45518/hamroapp'
     
 	
 }
